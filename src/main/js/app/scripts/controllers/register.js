4db20da8bc5880457b56b2e0db6e22f3b27e6a6f
Matthias Brun <matthias.brun@gmail.com>
2014-06-17 16:07:29 +0200

    Rename nickname to username

--- src/main/js/app/scripts/controllers/register.js
+++ src/main/js/app/scripts/controllers/register.js
@@ -12,20 +12,18 @@ worldcup.controller('RegisterCtrl', function ($scope, registration, Auth, $locat
         surname: ''
     };
 
-    $scope.rememberMe = false;
-
     $scope.error = '';
 
     $scope.hasError = function () {
         return $scope.error.length > 0;
     };
 
-    $scope.register.submitRegisterWithLogin = function(register) {
-        registration.register(register, function() {
+    $scope.register.submitRegister = function (register) {
+        registration.register(register, function () {
             var credentials = {
                 user: register.name,
                 password: register.password,
-                rememberMe: $scope.rememberMe
+                rememberMe: true
             };
             console.log('Successful registraion of user:' + register.name);
             Auth.login(credentials, function (user) {
@@ -34,17 +32,9 @@ worldcup.controller('RegisterCtrl', function ($scope, registration, Auth, $locat
             }, function (error) {
                 $scope.error = error;
             });
-        }, function(error) {
+        }, function (error) {
             $scope.error = error.data;
         });
     };
 
-    $scope.register.submitRegister = function (register) {
-        registration.register(register, function() {
-            console.log('Successful registraion of user:' + register.name);
-            $location.path('/login/');
-        }, function(error) {
-            $scope.error = error.data;
-        });
-    };
 });
