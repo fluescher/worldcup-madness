74e0abf3f970d7bdbfcf6cbe3da939290d12b728
Florian Luescher <florian@florianluescher.ch>
2014-06-17 17:54:06 +0200

    Remove static teams

--- src/main/scala/com/zuehlke/worldcup/core/model/Team.scala
+++ src/main/scala/com/zuehlke/worldcup/core/model/Team.scala
@@ -1,10 +1,3 @@
 package com.zuehlke.worldcup.core.model
 
 case class Team(name: String, flag: String, abbreviation: String)
-
-object Team {
-  val switzerland = Team("Schweiz", "", "CH")
-  val honduras = Team("Honduras", "", "HON")
-  val france = Team("Frankreich", "", "FRA")
-  val ecuador = Team("Ecuador", "", "ECU")
-}
