53ce82f809ac608909c46f2a2639fcdbd37e58b0
Florian Luescher <florian@florianluescher.ch>
2014-06-18 11:21:55 +0200

    New Formats

--- src/main/scala/com/zuehlke/worldcup/core/model/Game.scala
+++ src/main/scala/com/zuehlke/worldcup/core/model/Game.scala
@@ -1,5 +1,9 @@
 package com.zuehlke.worldcup.core.model
 
-case class Game(startTime: String, team1: Team, team2: Team, result: Option[GameResult]) 
+case class Game(startTime: String, round: Int,
+			    team1: Team, team2: Team, 
+			    result: Option[GameResult]) {
+  def gameId: String = s"${round}-${team1.abbreviation}-${team2.abbreviation}"
+} 
 
 case class GameResult(goalsTeam1: Int, goalsTeam2: Int)
--- src/main/scala/com/zuehlke/worldcup/core/model/User.scala
+++ src/main/scala/com/zuehlke/worldcup/core/model/User.scala
@@ -1,3 +1,5 @@
 package com.zuehlke.worldcup.core.model
 
-case class User(name: String, password: String, forename: String, surname: String, email: String) 
+case class User(name: String, password: String, 
+				forename: String, surname: String,
+				email: String) 
