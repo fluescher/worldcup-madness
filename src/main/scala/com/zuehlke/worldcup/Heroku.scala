5769ef8211647feb2ffc20de9fba5ce79ecc2890
Florian Luescher <florian@florianluescher.ch>
2014-06-19 13:58:33 +0200

    Really use heroku config

--- src/main/scala/com/zuehlke/worldcup/Heroku.scala
+++ src/main/scala/com/zuehlke/worldcup/Heroku.scala
@@ -4,5 +4,6 @@ import com.zuehlke.worldcup.core.BootedCore
 import com.zuehlke.worldcup.http.api.Api
 import com.zuehlke.worldcup.http.HttpServer
 import com.zuehlke.worldcup.config.LocalConfiguration
+import com.zuehlke.worldcup.config.HerokuConfiguration
 
-object Heroku extends App with LocalConfiguration with BootedCore with HttpServer
+object Heroku extends App with HerokuConfiguration with BootedCore with HttpServer
