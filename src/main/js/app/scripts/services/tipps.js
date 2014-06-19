1e91204592baeb8bfe2f0b4a6e1f06bc521af2bf
Matthias Brun <matthias.brun@gmail.com>
2014-06-19 13:21:10 +0200

    Add phone.css

--- src/main/js/app/scripts/services/tipps.js
+++ src/main/js/app/scripts/services/tipps.js
@@ -2,6 +2,6 @@
 
 worldcup.factory('Tipps', function ($resource) {
     return $resource('http://localhost:8080/api/tipps/:userId', {userId: '@id'}, {
-        'get' : {'method': 'GET', isArray: false}
+        'get' : {'method': 'GET', isArray: true}
     });
 });
