d91d7e775571b65ef88396a5ea813691ff16f2f6
Matthias Brun <matthias.brun@gmail.com>
2014-06-19 08:17:38 +0200

    Add tipps resource

--- src/main/js/app/scripts/controllers/games.js
+++ src/main/js/app/scripts/controllers/games.js
@@ -1,5 +1,30 @@
 'use strict';
 
-worldcup.controller('GamesCtrl', function ($scope, $http, Auth, Games) {
+worldcup.controller('GamesCtrl', function ($scope, $rootScope, Games, $http, Tipps, Auth) {
     $scope.games = Games.query();
+
+    $scope.save = function () {
+        var tip = {
+            user: Auth.getUser(),
+            goalsTeam1: 1,
+            goalsTeam2: 2,
+            gameId: '1-BRA-CRO'
+        };
+        console.log(angular.toJson(tip));
+
+        $http.post('http://localhost:8080/api/tipps', tip).success(function (data) {
+            console.log(data);
+        });
+    };
+
+    $rootScope.$on('Auth:login', function () {
+        console.log(Tipps.get({userId: 'test'}));
+        console.log(Auth.getUser());
+
+        $http.get('http://localhost:8080/api/tipps/' + Auth.getUser().name).success(function (data) {
+            console.log(data);
+            $scope.tipps = data;
+        });
+    });
+
 });
