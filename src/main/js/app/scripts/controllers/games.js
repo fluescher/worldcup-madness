d18700044f05199d7b79cb00767122514eacbefe
Matthias Brun <matthias.brun@gmail.com>
2014-06-19 11:27:21 +0200

    Add phone.css

--- src/main/js/app/scripts/controllers/games.js
+++ src/main/js/app/scripts/controllers/games.js
@@ -2,21 +2,6 @@
 
 worldcup.controller('GamesCtrl', function ($scope, Games, Tipps, Auth) {
 
-    $scope.save = function () {
-        var tip = {
-            user: Auth.getUser(),
-            goalsTeam1: 1,
-            goalsTeam2: 2,
-            gameId: '1-bra-cro'
-        };
-        console.log(angular.toJson(tip));
-
-        Tipps.save(tip, function () {
-            console.log('Saved tip');
-            $scope.tipps = Tipps.get({userId: Auth.getUser().name});
-        });
-    };
-
     Auth.getUser(function (user) {
         $scope.tipps = Tipps.get({userId: user.name}, function (tipps) {
             Games.query(function (games) {
