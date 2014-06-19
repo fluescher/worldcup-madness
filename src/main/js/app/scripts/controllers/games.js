86d1a4f55a0fea75fa0fabdb8c0149b926d7ff89
Matthias Brun <matthias.brun@gmail.com>
2014-06-19 10:54:20 +0200

    Save tip

--- src/main/js/app/scripts/controllers/games.js
+++ src/main/js/app/scripts/controllers/games.js
@@ -1,25 +1,58 @@
 'use strict';
 
 worldcup.controller('GamesCtrl', function ($scope, Games, Tipps, Auth) {
-    $scope.games = Games.query();
 
     $scope.save = function () {
         var tip = {
             user: Auth.getUser(),
             goalsTeam1: 1,
             goalsTeam2: 2,
-            gameId: '1-BRA-CRO'
+            gameId: '1-bra-cro'
         };
         console.log(angular.toJson(tip));
 
         Tipps.save(tip, function () {
-            console.log('Saved tip: ' + tip);
+            console.log('Saved tip');
+            $scope.tipps = Tipps.get({userId: Auth.getUser().name});
         });
     };
 
     Auth.getUser(function (user) {
-        console.log(user);
-        $scope.tipps = Tipps.get({userId: user.name});
+        $scope.tipps = Tipps.get({userId: user.name}, function (tipps) {
+            Games.query(function (games) {
+                angular.forEach(tipps, function (tip) {
+                    for(var i = 0; i < games.length; i++) {
+                        if (games[i].gameId === tip.gameId) {
+                            games[i].tip = tip;
+                            break;
+                        }
+                    }
+                });
+                $scope.games = games;
+            });
+        });
     });
+
+    $scope.saveTip = function(game) {
+        if (!angular.isNumber(game.tip.goalsTeam1) || game.tip.goalsTeam1 < 0){
+            console.log('Goals Team 1 is not a number or negative.');
+            return;
+        }
+        if (!angular.isNumber(game.tip.goalsTeam2) || game.tip.goalsTeam2 < 0){
+            console.log('Goals Team 2 is not a number or negative.');
+            return;
+        }
+
+        var tip = {
+            user: Auth.getUser(),
+            goalsTeam1: game.tip.goalsTeam1,
+            goalsTeam2: game.tip.goalsTeam2,
+            gameId: game.gameId
+        };
+
+        Tipps.save(tip, function () {
+            console.log('Saved tip');
+        });
+    };
 
 });
