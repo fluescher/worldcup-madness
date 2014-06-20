2b5a31ac50cadddf2d8ad4dab27f246ad90d8901
Florian Luescher <florian@florianluescher.ch>
2014-06-20 19:52:10 +0200

    more logs

--- src/main/scala/com/zuehlke/worldcup/core/model/Game.scala
+++ src/main/scala/com/zuehlke/worldcup/core/model/Game.scala
@@ -12,7 +12,7 @@ case class Game(startTime: String, round: Int,
   def tippsAccepted(dateTime: DateTime): Boolean = {
     val format = DateTimeFormat.forPattern("yyyy/MM/dd")
     val startDate = format.parseDateTime(startTime)
-    println(s"$this: comparing $dateTime with $startDate")
+    println(s"$this: comparing $dateTime with ${startDate.withZone(myZone) + 18.hours}")
     startDate.withZone(myZone) + 18.hours > dateTime
   }
   
