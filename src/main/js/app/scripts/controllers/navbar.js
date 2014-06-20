96986fabba80344a46fa3d99d135a2175576f6be
Pascal Kaufmann <me@example.com>
2014-06-20 10:30:15 +0200

    enhance navbar

--- src/main/js/app/scripts/controllers/navbar.js
+++ src/main/js/app/scripts/controllers/navbar.js
@@ -2,11 +2,16 @@
 
 worldcup.controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.navClass = function (page) {
+        console.log('#########################################');
         var currentRoute = $location.path().substring(1) || 'games';
+        page = page === 'user/' ? page + $scope.user.name : page;
+        console.log(currentRoute);
+        console.log(page);
         return page === currentRoute ? 'active' : '';
     };
 
     $scope.isLoggedIn = function () {
+        console.log($scope.user.name);
         return Auth.isLoggedIn();
     };
 
@@ -14,7 +19,7 @@ worldcup.controller('NavbarCtrl', function ($scope, $location, Auth) {
         Auth.logout();
     };
 
-    Auth.getUser(function(user){
+    Auth.getUser(function(user) {
         $scope.user = user;
     });
 
